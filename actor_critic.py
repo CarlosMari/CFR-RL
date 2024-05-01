fb7968c5dbc3d5f2557e2ad49f4693899584042f
Carlos Marí <202009279@alu.comillas.edu>
2024-05-01 13:55:17 -0500

    Actor Critic

--- actor_critic.py
+++ actor_critic.py
@@ -14,28 +14,58 @@ class ActorCriticModel(Model):
         self.config = config
         self.input_dim = input_dim
         self.action_dim = action_dim
-        self.Conv2D_out = config.Conv2D_out  # Why are this parameters
+        self.Conv2D_out = config.Conv2D_out 
         self.Dense_out = config.Dense_out
         self.max_moves = max_moves
-        # print(f'Dimensions : {self.input_dim}')
-        self.actor = nn.Sequential(
-            nn.Conv2d(self.input_dim[2], self.Conv2D_out, kernel_size=3, padding=1),
+
+        self.a_conv_block = nn.Sequential(
+            nn.Conv2d(1, self.Conv2D_out, kernel_size=3, padding=1),
             nn.LeakyReLU(),
             nn.Flatten(),
-            nn.Linear(self.Conv2D_out * self.input_dim[0] * self.input_dim[1], self.Dense_out),
+            nn.Linear(self.Conv2D_out * 12 * 12, 500),
             nn.LeakyReLU(),
-            nn.Linear(self.Dense_out, self.action_dim),
+            nn.Linear(500, 288),
+            nn.LeakyReLU()
+        )
+
+        self.a_conv_block2 = nn.Sequential(
+            nn.Conv2d(1, 128, kernel_size=3, padding=1),
+            nn.LeakyReLU(),
+            nn.Flatten(),
+            nn.Linear(128 * 12 * 12, 288),
+            nn.LeakyReLU(),
+        )
+        
+        self.c_conv_block = nn.Sequential(
+                    nn.Conv2d(1, self.Conv2D_out, kernel_size=3, padding=1),
+                    nn.LeakyReLU(),
+                    nn.Flatten(),
+                    nn.Linear(self.Conv2D_out * 12 * 12, 500),
+                    nn.LeakyReLU(),
+                    nn.Linear(500, 288),
+                    nn.LeakyReLU()
+                )
+
+        self.c_conv_block2 = nn.Sequential(
+            nn.Conv2d(1, 128, kernel_size=3, padding=1),
+            nn.LeakyReLU(),
+            nn.Flatten(),
+            nn.Linear(128 * 12 * 12, 288),
+            nn.LeakyReLU(),
+        )
+
+        self.actor = nn.Sequential(
+            nn.Linear(288*2,288, dtype=torch.float64),
+            nn.LeakyReLU(),
+            nn.Linear(288, action_dim, dtype=torch.float64)
         )
 
         self.critic = nn.Sequential(
-            nn.Conv2d(self.input_dim[2], self.Conv2D_out, kernel_size=3, padding=1),
+            nn.Linear(288*2,288, dtype=torch.float64),
             nn.LeakyReLU(),
-            nn.Flatten(),
-            nn.Linear(self.Conv2D_out * self.input_dim[0] * self.input_dim[1], self.Dense_out),
-            nn.LeakyReLU(),
-            nn.Linear(self.Dense_out, 1)
-
+            nn.Linear(288, 1, dtype=torch.float64)
         )
+
 
         # We create the optimizers
         self.initialize_optimizers()
@@ -73,20 +103,54 @@ class ActorCriticModel(Model):
         self.lr_scheduler_critic.step()
         self.lr_scheduler_actor.step()
 
-    def forward(self, inputs):
+    def forward(self, inputs, mat):
         # print("Input type: ")
         # print(type(inputs))
-        logits = self.actor(inputs)
+        inputs = inputs.to(torch.float32).to(self.device)
+        mat = mat.to(torch.float32).to(self.device)
+
+        if len(mat.shape) == 1:
+            mat = F.normalize(mat, dim=0)
+            mat = mat.reshape(12, 12).unsqueeze(0).unsqueeze(0)
+        else:
+            mat = mat.reshape(mat.shape[0], mat.shape[1] * mat.shape[2])  # (B, 12^2)
+            mat = F.normalize(mat, dim=1)
+            mat = mat.reshape(mat.shape[0], 12, 12)  # (B,12,12)
+            mat = mat.unsqueeze(0).permute(1, 0, 2, 3)  # (B,C,12,12)
+
+        assert mat.shape == inputs.shape, f'{mat.shape},{inputs.shape}'
+        x = self.a_conv_block(inputs)
+        x_2 = self.a_conv_block2(mat)
+        final_x = torch.cat((x, x_2), dim=1).to(torch.double)  # [B,288]
+
+        x_c = self.c_conv_block(inputs)
+        x_2_c = self.c_conv_block2(mat)
+        final_x_c = torch.cat((x_c, x_2_c), dim=1).to(torch.double)  # [B,288]
+
+        logits = self.actor(final_x)
+        value = self.critic(final_x_c)
+        if self.config.logit_clipping > 0:
+            logits = self.config.logit_clipping * torch.tanh(logits)
+        # Returns logits, policy
+        policy = F.softmax(logits, dim=1)
+
+        return logits, value, policy
+
+
+
+
+        """logits = self.actor(inputs)
         logits = logits.to(torch.float64)
         if self.config.logit_clipping > 0:
             logits = self.config.logit_clipping * torch.tanh(logits)
 
         policy = F.softmax(logits, dim=1)
         values = self.critic(inputs)
-        return logits, values, policy
+        return logits, values, policy"""
 
-    def _train(self, inputs, actions, rewards, entropy_weight=0.01):
+    def _train(self, inputs, actions, rewards, entropy_weight=0.01, mats=None):
         # We make sure the vectors are pytorch vectors
+        mats = torch.stack(mats, dim=0).to(self.device)
         inputs = torch.stack(inputs, dim=0)
         # print(inputs.shape)
         # rewards = torch.tensor(rewards, dtype=torch.float64)
@@ -102,7 +166,7 @@ class ActorCriticModel(Model):
 
         # We call the forward function and calculate the loss
         # [B, C_in, H, W]
-        logits, values, policy = self(inputs.permute(0, 3, 1, 2))
+        logits, values, policy = self(inputs.permute(0, 3, 1, 2), mats)
 
         # We calculate loss and advantages
         value_loss, advantages = self.value_loss_fn(rewards, values)
@@ -116,10 +180,8 @@ class ActorCriticModel(Model):
 
         # Value_loss, entropy, actor gradients and critic gradients.
         # We return the gradients for assert
-        return value_loss.item(), entropy, [param.grad for param in self.actor.parameters()], [param.grad for
-                                                                                               param in
-                                                                                               self.critic.parameters()]
-
+        return value_loss.item(), entropy
+    
     def predict(self, input):
         """
 
