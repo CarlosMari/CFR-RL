d682fbb8662f474a12089db86a358331239d1a33
Carlos Marí <202009279@alu.comillas.edu>
2024-05-09 15:20:56 -0500

    Frontera settings

---
+++ fontera/runserial.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH -J launcher-test            # job name
+#SBATCH -o launcher.o%j             # output and error file name (%j expands to SLURM jobID)
+#SBATCH -N 1                        # number of nodes requested
+#SBATCH -n 2                        # total number of tasks to run in parallel
+#SBATCH -p development              # queue (partition) 
+#SBATCH -t 00:30:00                 # run time (hh:mm:ss) 
+#SBATCH --mail-type=all             # Send email at begin and end of job
+#SBATCH -A MLL                      # Project/Allocation name (req'd if you have more than 1)
+#SBATCH --mail-user=carlosmari@utexas.edu
+
+module load launcher
+module load python3/3.9.2
+source ../python-envs/CFR-RL/bin/activate
+
+export LAUNCHER_WORKDIR=/scratch1/09701/cmari/test
+export LAUNCHER_JOB_FILE=jobrun 
+
+${LAUNCHER_DIR}/paramrun
